4b3cedd4b6bcfa17315565fde0a058de91eecc49
Amaro018 <gladi0018@gmail.com>
2024-12-13 12:10:46 +0800

    fixing the history payment

--- src/app/queries/getPaymentByRentId.ts
+++ src/app/queries/getPaymentByRentId.ts
@@ -3,6 +3,7 @@ import db from "db"
 export default async function getPaymentByRentId({ rentItemId }: { rentItemId: number }) {
   const payments = await db.payment.findMany({
     where: { rentItemId },
+    include: { rentItem: true },
   })
 
   if (!payments) throw new Error("No payments found")
