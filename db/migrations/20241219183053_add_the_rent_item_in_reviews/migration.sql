45e3428ee44290f926c07a1e6affb2219a0e5739
Amaro018 <gladi0018@gmail.com>
2024-12-20 02:55:48 +0800

    fixing the reviews

---
+++ db/migrations/20241219183053_add_the_rent_item_in_reviews/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - Added the required column `rentItemId` to the `Review` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Review" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "productId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "rentItemId" INTEGER NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "comment" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Review_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Review_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Review_rentItemId_fkey" FOREIGN KEY ("rentItemId") REFERENCES "RentItem" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Review" ("comment", "createdAt", "id", "productId", "rating", "updatedAt", "userId") SELECT "comment", "createdAt", "id", "productId", "rating", "updatedAt", "userId" FROM "Review";
+DROP TABLE "Review";
+ALTER TABLE "new_Review" RENAME TO "Review";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
