98c946f6a1be6f6b707890376beaf9020efc4fc1
Amaro018 <gladi0018@gmail.com>
2024-12-26 19:02:32 +0800

    fixing the admin cards

---
+++ src/app/admin/components/AdminCards.tsx
@@ -0,0 +1,32 @@
+export default function AdminCards() {
+    return(
+        <>
+
+        <div className="flex flex-row gap-4 w-full">
+
+        <div className="flex flex-col items-center p-2 shadow-lg shadow-slate-500 rounded-lg w-full">
+        <h1>Pending Shops</h1>
+        <p>1</p>
+        </div>
+
+        <div className="flex flex-col items-center p-2 shadow-lg shadow-slate-500 rounded-lg w-full">
+        <h1>Approved Shops</h1>
+        <p>1</p>
+        </div>
+
+        <div className="flex flex-col items-center p-2 shadow-lg shadow-slate-500 rounded-lg w-full">
+        <h1>Rejected Shops</h1> 
+        <p>1</p>
+        </div>
+
+
+        <div className="flex flex-col items-center p-2 shadow-lg shadow-slate-500 rounded-lg w-full">
+        <h1>Rejected Shops</h1> 
+        <p>1</p>
+        </div>
+
+
+        </div>
+        </>
+    )
+}
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,7 +2,7 @@ import { invoke } from "./../blitz-server"
 import getCurrentUser from "./../users/queries/getCurrentUser"
 import { Sidebar } from "./components/sidebar"
 import Navbar from "../components/Navbar"
-
+import AdminCards from "./components/AdminCards"
 export default async function Page() {
   const currentUser = await invoke(getCurrentUser, null)
   return (
@@ -11,7 +11,9 @@ export default async function Page() {
         <div className="w-64 ">
           <Sidebar currentUser={currentUser} />
         </div>
-        <div className="w-full px-16">admin</div>
+        <div className="w-full p-16">
+            <AdminCards />
+        </div>
       </main>
     </div>
   )
